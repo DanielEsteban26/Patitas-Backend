41321cb053fed8d0008f98b2ec35458901539736
Daniel Isidro <isidroesteban402@gmail.com>
2024-10-12 12:55:48 -0500

    patitas-back casi finalizado

--- src/main/java/pe/edu/cibertec/patitas_backend_a/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_a/controller/AutenticacionController.java
@@ -1,15 +1,18 @@
 package pe.edu.cibertec.patitas_backend_a.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.patitas_backend_a.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_backend_a.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_backend_a.dto.LogoutRequestDTO;
+import pe.edu.cibertec.patitas_backend_a.dto.LogoutResponseDTO;
 import pe.edu.cibertec.patitas_backend_a.service.AutenticacionService;
+import pe.edu.cibertec.patitas_backend_a.service.impl.LogoutServiceImpl;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 
@@ -20,11 +23,14 @@ public class AutenticacionController {
     @Autowired
     AutenticacionService autenticacionService;
 
+    @Autowired
+    private LogoutServiceImpl logoutService;
+
     @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
 
         try {
-            Thread.sleep(Duration.ofSeconds(5));
+            Thread.sleep(Duration.ofSeconds(1));
             String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
             System.out.println("Resultado: " + Arrays.toString(datosUsuario));
             if (datosUsuario == null) {
@@ -38,4 +44,18 @@ public class AutenticacionController {
 
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<LogoutResponseDTO> logout(@RequestBody LogoutRequestDTO logoutRequestDTO) {
+        try {
+
+            logoutService.logout(logoutRequestDTO);
+
+            return ResponseEntity.ok(new LogoutResponseDTO("00", "Cierre de Sesión exitoso",logoutRequestDTO.tipoDocumento(), logoutRequestDTO.numeroDocumento()));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new LogoutResponseDTO("01", "Error: " + e.getMessage(), "", ""));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(new LogoutResponseDTO("99", "Error: Ocurrió un problema en el cierre de sesión", "", ""));
+        }
+    }
 }
---
+++ src/main/java/pe/edu/cibertec/patitas_backend_a/service/LogoutService.java
@@ -0,0 +1,7 @@
+package pe.edu.cibertec.patitas_backend_a.service;
+
+import pe.edu.cibertec.patitas_backend_a.dto.LogoutRequestDTO;
+
+public interface LogoutService {
+    void logout(LogoutRequestDTO logoutRequest); // Cambia a LogoutRequestDTO
+}
